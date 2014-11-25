cc4b9234bea95a45f7efe1090e09d4243da6456e
Choongsoon Chang <choongsoonc@gmail.com>
2014-11-25 12:33:48 -0800

    Added sqrt.go

---
+++ go-examples/src/github.com/choongsoon/hello/hello.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+    "fmt"
+    "github.com/choongsoon/newmapth"
+)
+
+func main() {
+    fmt.Printf("hello, world.\n")
+    fmt.Printf("My Sqrt(2) = %v\n", newmath.Sqrt(2))
+}
